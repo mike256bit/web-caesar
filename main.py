07e19bd37d80f6c8d2654cf8a29668990d4914f4
mike256bit <mike.toth@gmail.com>
2018-09-21 20:49:56 -0400

    Add return as box under form

--- main.py
+++ main.py
@@ -10,40 +10,41 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                .box {{
                     background-color: #eee;
                     padding: 20px;
-                    margin: 0 auto;
+                    margin: 5px auto;
                     width: 80%;
-                    font: 16px sans-serif;
+                    font: 14px sans-serif;
                     border-radius: 10px;
                     text-align: center;
-                }
+                }}
 
-                textarea {
+                textarea {{
                     margin-top: 10px;
                     margin-bottom: 20px;
                     width: 80%;
                     height: 120px;
-                }
+                }}
 
-                .inputs {
+                .inputs {{
                     background-color: lightgray;
+                    font-size: 16px;
                     font-weight: bold;
                     width: 400px; 
                     padding: 10px;
                     margin: 10px;
                     border-radius: 10px;
                     display: inline-block;
-                }
+                }}
 
-                .rotation {
+                .rotation {{
                     width: 20%;
                     margin-left: 10px;
                     padding-left: 10px; 
-                }
+                }}
 
-                button {
+                button {{
                     width: 100px;
                     margin: 10px auto;
                     border: 1px solid #864100;
@@ -53,33 +54,34 @@ form = """
                     transition-duration: 0.2s
                     display: inline-block;
                     letter-spacing: 1px;
-                }
+                }}
 
-                button:hover {
+                button:hover {{
                     background-color: #864100;
                     color: white;
                     font-weight: bold;
-                }
+                }}
 
-                h1 {
+                h1 {{
                     text-align: center;
                     font: 20px sans-serif;
                     color: #864100;
-                }
+                }}
 
-                h2 {
+                h2 {{
                     text-align: center;
                     font: 14px sans-serif;
                     font-style: italic;
                     margin: -10px 0 10px 0; 
-                }
+                }}
+
 
             </style>
         </head>
         <body>
             <h1>Web-Caesar Encryption</h1>
             <h2>Michael Toth - LC101 Unit 2</h2>
-            <form action="/" method="POST">
+            <form class="box" action="/" method="POST">
                 <div class="inputs">
 
                     Encryption rotation (number): 
@@ -87,7 +89,7 @@ form = """
                 
                 </div>
 
-                <textarea name="text">Sample text.</textarea>
+                <textarea name="text">{0}</textarea>
 
                 <div style="text-align: center;">
                     <button type="submit">ENCRYPT</button> <button type="reset">RESET</button>
@@ -95,48 +97,8 @@ form = """
 
             </form>
 
-        </body>
-    </html>
-    """
-encrypted = """
-    <html>
-        <head>
-            <style>
-                div {
-                    background-color: #eee;
-                    padding: 20px;
-                    margin: 0 auto;
-                    width: 80%;
-                    font: 16px sans-serif;
-                    border-radius: 10px;
-                    text-align: center;
-                }
-
-                h1 {
-                    text-align: center;
-                    font: 20px sans-serif;
-                    color: #864100;
-                    margin-top: 13px;
-                }
-
-                h2 {
-                    text-align: center;
-                    font: 14px sans-serif;
-                    font-style: italic;
-                    margin: -10px 0 10px 0; 
-                }
-
-            </style>
-        </head>
-        <body>
-            <h1>Web-Caesar Encryption</h1>
-            <h2>Michael Toth - LC101 Unit 2</h2>
-            <div>
-
-
-    """
-
-encrypted_footer = """
+            <div class="box">
+                {1}
             </div>
         </body>
     </html>
@@ -144,16 +106,16 @@ encrypted_footer = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format("Sample Text.", "Test Stuff")
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = str(request.form['text'])
 
-    return_text = encrypted+"<p>"+str(rotate_string(text, rot))+"</p>"+encrypted_footer
+    # return_text = encrypted+"<p>"+str(rotate_string(text, rot))+"</p>"+encrypted_footer
 
-    return return_text
+    return form.format("Sample Text.", str(rotate_string(text, rot)))
 
 
 app.run()
